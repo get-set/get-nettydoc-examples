f97d702ca8982580ebad3f5b2c2733a1ee949884
get-set <535993116@qq.com>
2018-04-04 15:15:42 +0800

    Update comments: fundamental-discard

--- fundamental-discard/src/main/java/com/getset/nettyex/discard/DiscardServer.java
+++ fundamental-discard/src/main/java/com/getset/nettyex/discard/DiscardServer.java
@@ -52,7 +52,7 @@ public class DiscardServer {
                         }
                     });
 
-            // 启动EchoServer
+            // 启动 server
             ChannelFuture future = serverBootstrap.bind(PORT).sync();
             // 等待server socket 关闭
             future.channel().closeFuture().sync();
